0bfea75e30b1b09627bfc7f4103b4e32c8ad129d
yehia abdulhamid <yehiamcp@gmail.com>
2017-06-22 21:49:26 +0300

    remove useless test case

--- tests/test_timezones.php
+++ tests/test_timezones.php
@@ -5,32 +5,6 @@ use App\Models\Schedule;
 
 class timezone_test extends TestCase
 {
-    public function test_creating_from_another_timezone_case_new_york()
-    {
-        $sourceTzStr = "America/New_York";
-        $sourceDateTimeStr = "2017-12-08 08:00 PM";
-
-        $distinationTzStr = "";
-        $distinationDateTimeStr = "";
-
-        $expectedStr = "";
-
-        $this->assertEquals(0, 0);
-    }
-
-    public function test_creating_from_another_timezone_case_morovia()
-    {
-        $sourceTzStr = "Africa/Monrovia";
-        $sourceDateTimeStr = "2017-12-08 08:00 PM"; //10:00 PM
-
-        $distinationTzStr = "";
-        $distinationDateTimeStr = "";
-
-        $expectedStr = "";
-
-        // $this->assertEquals(0, 0);
-    }
-
     public function test_textualDayDateConvertion()
     {
         // Thursday, 8 Dec 08:00 PM //time zone newyork
